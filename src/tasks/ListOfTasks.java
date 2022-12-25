74221272db831ecb0639ca8349f6005883aeeebb
Acer <oduvan4ik_888@mail.ru>
2022-12-25 21:08:11 +0300

    Исправлено

--- src/tasks/ListOfTasks.java
+++ src/tasks/ListOfTasks.java
@@ -2,27 +2,27 @@ package tasks;
 import java.time.LocalDate;
 import java.util.*;
 public class ListOfTasks {
-   private final Map<Integer ,Task> MAP_OF_TASKS = new HashMap<>();
+   private final Map<Integer ,Task>  mapOfTasks = new HashMap<>();
     public void addTasks (Task task){
-        this.MAP_OF_TASKS.put(task.getId(), task);
+        this. mapOfTasks.put(task.getId(), task);
     }
     public void removeTask (int id){
-        this.MAP_OF_TASKS.remove(id);
+        this. mapOfTasks.remove(id);
     }
     
     public int getSizeTask (){
-        return MAP_OF_TASKS.size();
+        return  mapOfTasks.size();
     }
 
     public List<Task> getCurrnetTasks(LocalDate valueDate){
         List<Task> currentTasks = new ArrayList<>();
-        for (Task task: MAP_OF_TASKS.values()) {
+        for (Task task:  mapOfTasks.values()) {
             if (task.isCurrentTask(valueDate)) { currentTasks.add(task);}
         }
         return currentTasks;
     }
 
     public Collection<Task> getTask(){
-        return this.MAP_OF_TASKS.values();
+        return this. mapOfTasks.values();
     }
 }
